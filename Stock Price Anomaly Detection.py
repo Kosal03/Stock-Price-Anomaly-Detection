facb34f6b553017fc0e04b0ad167e6e4fcee6ba1
KOSAL RAM <80051855+Kosal03@users.noreply.github.com>
2021-08-07 15:50:37 +0530

    changed

--- Stock Price Anomaly Detection.py
+++ Stock Price Anomaly Detection.py
@@ -108,5 +108,3 @@ anomalies = anomaly_df.loc[anomaly_df['anomaly'] == True]
 #Plot anomaly
 sns.lineplot(x=anomaly_df['Date'], y=scaler.inverse_transform(anomaly_df['Close']))
 sns.scatterplot(x=anomalies['Date'], y=scaler.inverse_transform(anomalies['Close']), color='r')
-
-#sjdfb 
